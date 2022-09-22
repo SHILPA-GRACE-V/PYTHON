f43f1a425ef1dc121b5a9e38132166bc09339d27
SHILPA-GRACE-V <sgv3009@gmail.com>
2022-09-22 22:23:22 +0530

    commited

--- StudentMngmtSystem.py
+++ StudentMngmtSystem.py
@@ -50,38 +50,39 @@ obj = Student('', 0, 0, 0)
 
 print("\nOperations used, ")
 print("\n1.Accept Student details\n2.Display Student Details\n3.Search Details of a Student\n4.Delete Details of Student\n5.Update Student Details\n6.Exit")
+while True:
+    ch = int(input("Enter choice:"))
+    if(ch == 1):
+        obj.accept("A", 1, 100, 100)
+        obj.accept("B", 2, 90, 90)
+        obj.accept("C", 3, 80, 80)
 
-ch = int(input("Enter choice:"))
-if(ch == 1):
-    obj.accept("A", 1, 100, 100)
-    obj.accept("B", 2, 90, 90)
-    obj.accept("C", 3, 80, 80)
+    elif(ch == 2):
+        print("\n")
+        print("\nList of Students\n")
+        for i in range(ls.__len__()):
+	        obj.display(ls[i])
 
-elif(ch == 2):
-    print("\n")
-    print("\nList of Students\n")
-    for i in range(ls.__len__()):
-	    obj.display(ls[i])
+    elif(ch == 3):
+        print("\n Student Found, ")
+        s = obj.search(2)
+        obj.display(ls[s])
 
-elif(ch == 3):
-    print("\n Student Found, ")
-    s = obj.search(2)
-    obj.display(ls[s])
+    elif(ch == 4):
+        obj.delete(2)
+        print(ls.__len__())
+        print("List after deletion")
+        for i in range(ls.__len__()):
+	        obj.display(ls[i])
 
-elif(ch == 4):
-    obj.delete(2)
-    print(ls.__len__())
-    print("List after deletion")
-    for i in range(ls.__len__()):
-	    obj.display(ls[i])
+    elif(ch == 5):
+        obj.update(3, 2)
+        print(ls.__len__())
+        print("List after updation")
+        for i in range(ls.__len__()):
+	        obj.display(ls[i])
 
-elif(ch == 5):
-    obj.update(3, 2)
-    print(ls.__len__())
-    print("List after updation")
-    for i in range(ls.__len__()):
-	    obj.display(ls[i])
-
-else:
-    print("Thank You !")
+    else:
+        print("Thank You !")
+        break
 
